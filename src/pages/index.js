fcfc3a4c84f58afee31bdb05a1eadb30de4b6e9d
Amirulmath <amirulmath760@gmail.com>
2023-07-29 09:39:00 +0600

    Installed Ant Design and used

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
+import { Button } from "antd";
+
 const HomePage = () => {
   return (
     <div>
       <h1 className="text-green-600">Home Page</h1>
+      <Button type="primary">Pc Component</Button>
     </div>
   );
 };
